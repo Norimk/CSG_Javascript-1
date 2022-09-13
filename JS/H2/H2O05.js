2820dd1c5b2bfaa0fab24c5ae4092a4c6eff1526
Norimk <153037@csgaugustinus.nl>
2022-09-13 16:49:56 +0000

    h

--- JS/H2/H2O05.js
+++ JS/H2/H2O05.js
@@ -3,38 +3,38 @@ var aantalKolommenRaster = 9;
 var celGrootte;
 
 var spriteJos;
-var xJos;
-var yJos;
+var xJos = 400;
+var yJos = 300;
 
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
   spriteJos = loadImage("images/sprites/Jos100px/Jos_0.png");
+
 }
 
 function setup() {
-  canvas = createCanvas(901,601);
+  canvas = createCanvas(900,600);
   canvas.parent('processing');
   celGrootte = width / aantalKolommenRaster;
 }
 
 function draw() {
- 
+    background(brug);
     tekenRaster();
-  }
- 
+    image(spriteJos,xJos,yJos);
 }
+ 
+
 
 function tekenRaster() {
   push();
   noFill();
   stroke('grey');
-  /*
-  Maak hieronder een dubbele herhaling om een raster te maken.
-  HINT: je kunt terugkijken naar het raster dat je in H1 hebt gemaakt.
-  Maak gebruik van de variabelen die bovenaan zijn gedeclareerd.
-  */
-  for(var n=1; n=6; n++) {
-      rect(4*celGrootte,2*celGrootte,celGrootte,celGrootte);
-  }
+
+  for (var rij= 0;rij < aantalRijenRaster;rij++) {
+   for (var kolom = 0;kolom < aantalKolommenRaster;kolom++) {
+      rect(kolom*celGrootte,rij*celGrootte,celGrootte,celGrootte);
+   }
+    }
   pop();
 }
