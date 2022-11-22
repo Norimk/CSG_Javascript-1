998e2943076a43401b63075bb96da314f89b6564
Norimk <153037@csgaugustinus.nl>
2022-11-22 13:16:03 +0000

    v

--- JS/HOBF/HOBFO01.js
+++ JS/HOBF/HOBFO01.js
@@ -1,3 +1,228 @@
-// deze code is onleesbaar gemaakt met https://obfuscator.io
+// var strand;
+// var strandX = 0;
 
-var _0x2493=['\x6a\x65\x61\x59\x4a','\x6d\x57\x56\x64\x77','\x63\x66\x41\x53\x52','\x70\x72\x6f\x63\x65\x73\x73\x69\x6e\x67','\x50\x41\x6a\x55\x6d','\x68\x62\x7a\x4a\x69','\x54\x46\x4c\x48\x73','\x32\x7c\x36\x7c\x31\x7c\x33\x7c\x37\x7c\x35\x7c\x30\x7c\x34','\x56\x4a\x44\x71\x49','\x73\x70\x6c\x69\x74','\x71\x4d\x64\x4b\x70','\x6b\x7a\x6d\x6c\x58'];(function(_0x1152ce,_0x578178){var _0x10f684=function(_0x3f06cc){while(--_0x3f06cc){_0x1152ce['push'](_0x1152ce['shift']());}};_0x10f684(++_0x578178);}(_0x2493,0x12f));var _0x4a3a=function(_0x515f86,_0x5af499){_0x515f86=_0x515f86-0x0;var _0x4a759a=_0x2493[_0x515f86];return _0x4a759a;};var diameter=0x12c;var d=0x0;function setup(){var _0x4a2b65={'NYpwg':function(_0x3f147a,_0x11d323,_0x9b08a1){return _0x3f147a(_0x11d323,_0x9b08a1);},'PAjUm':_0x4a3a('0x0'),'hbzJi':function(_0x23fd05,_0x2e936c,_0x8b6a6e,_0x521324,_0x22cce3,_0x4b8973){return _0x23fd05(_0x2e936c,_0x8b6a6e,_0x521324,_0x22cce3,_0x4b8973);},'TFLHs':function(_0x22a8dd){return _0x22a8dd();}};var _0x208e3e=_0x4a2b65['\x4e\x59\x70\x77\x67'](createCanvas,0x1c2,0x1c2);_0x208e3e['\x70\x61\x72\x65\x6e\x74'](_0x4a2b65[_0x4a3a('0x1')]);_0x4a2b65[_0x4a3a('0x2')](colorMode,RGB,0xff,0xff,0xff,0x1);_0x4a2b65[_0x4a3a('0x3')](noStroke);}function draw(){var _0x2f6831={'VJDqI':_0x4a3a('0x4'),'LoHig':function(_0x2d9e7c,_0x25bcc1,_0x242790,_0x9dd858,_0x18c16f){return _0x2d9e7c(_0x25bcc1,_0x242790,_0x9dd858,_0x18c16f);},'qMdKp':function(_0x445460,_0x136aa9){return _0x445460/_0x136aa9;},'kzmlX':function(_0x40bb40,_0x340ab9){return _0x40bb40-_0x340ab9;},'jeaYJ':function(_0x55b930,_0x33a031,_0x23c273,_0x724316,_0x23db7e){return _0x55b930(_0x33a031,_0x23c273,_0x724316,_0x23db7e);},'mWVdw':function(_0x515116,_0x2cdb37,_0x3f36e5,_0xde3a8,_0x264679){return _0x515116(_0x2cdb37,_0x3f36e5,_0xde3a8,_0x264679);},'cfASR':function(_0x57d819,_0x53e7c5){return _0x57d819/_0x53e7c5;}};var _0x265c62=_0x2f6831[_0x4a3a('0x5')][_0x4a3a('0x6')]('\x7c'),_0x137c65=0x0;while(!![]){switch(_0x265c62[_0x137c65++]){case'\x30':_0x2f6831['\x4c\x6f\x48\x69\x67'](ellipse,_0x2f6831[_0x4a3a('0x7')](width,0x2),_0x2f6831[_0x4a3a('0x8')](height,d),diameter,diameter);continue;case'\x31':_0x2f6831[_0x4a3a('0x9')](ellipse,_0x2f6831[_0x4a3a('0x7')](width,0x2),_0x2f6831['\x71\x4d\x64\x4b\x70'](height,0x2),diameter,diameter);continue;case'\x32':_0x2f6831[_0x4a3a('0x9')](background,0xff,0xff,0xff,0x1);continue;case'\x33':_0x2f6831[_0x4a3a('0x9')](fill,0x0,0xff,0x0,0.5);continue;case'\x34':d++;continue;case'\x35':_0x2f6831[_0x4a3a('0x9')](fill,0x0,0x0,0xff,0.5);continue;case'\x36':_0x2f6831['\x6d\x57\x56\x64\x77'](fill,0xff,0x0,0x0,0.5);continue;case'\x37':_0x2f6831[_0x4a3a('0xa')](ellipse,_0x2f6831[_0x4a3a('0xb')](width,0x2),d,diameter,diameter);continue;}break;}}
+// function preload() {
+//   strand = loadImage("https://www.ufomeldpunt.nl/storage/schetsen/ufo-2849.png");
+// }
+
+// function setup() {
+//   canvas = createCanvas(600,400);
+//   canvas.parent('processing');
+// }
+
+// function draw() {
+//   background('grey');
+//   image(strand,strandX,0);
+//   image(strand,strandX + strand.width,0);
+//   strandX--;
+//   if (strandX == -strand.width) {
+//     strandX = 0;
+//   }  
+// }
+
+
+
+
+
+
+  
+
+class Bom {
+  constructor() {
+    this.x = floor(random(1,raster.aantalKolommen))*raster.celGrootte;
+    this.y = floor(random(0,raster.aantalRijen))*raster.celGrootte;
+  }
+  
+  toon() {
+    image(beloning,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+class Jos {
+  constructor() {
+    this.x = 0;
+    this.y = 200;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null;
+    this.gehaald = false;
+    this.aanDeBeurt = true;
+    this.staOpBom = false;
+  }
+  
+  beweeg() {
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+      this.aanDeBeurt = false;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+      this.aanDeBeurt = false;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+      this.aanDeBeurt = false;
+    }
+    
+    this.x = constrain(this.x,0,canvas.width);
+    this.y = constrain(this.y,0,canvas.height-raster.celGrootte);
+    
+    if (this.x == canvas.width) {
+      this.gehaald = true;
+    }
+  }
+  
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  staatOp(bommenLijst) {
+    for(var b = 0; b < bommenLijst.length; b++){
+      if(bommenLijst[b].x == this.x && bommenLijst[b].y == this.y){
+        this.staOpBom = true;
+      }
+    }
+    return this.staOpBom;
+  }      
+  
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+
+
+}
+
+function preload() {
+  beloning = loadImage("images/sprites/appel_2.png");
+}
+
+var bommenArray = [];
+
+function setup() {
+  canvas = createCanvas(900,600);
+  canvas.parent('processing');
+  frameRate(10);
+  textFont("Verdana");
+  textSize(90);
+  raster = new Raster(12,18);
+  raster.berekenCelGrootte();
+  bom1 = new Bom();
+
+  for (var b = 0;b < 5;b++) {
+    bommenArray.push(new Bom());
+  }
+  
+  eve = new Jos();
+  eve.stapGrootte=1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
+  }
+  
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
+
+  bob = new Vijand(600,400);
+  bob.stapGrootte = 1*eve.stapGrootte;
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");  
+
+
+
+  
+}
+
+function draw() {
+  background('black');
+  bom1.toon();
+
+  for (var b = 0;b < bommenArray.length;b++) {
+    bommenArray[b].toon();
+  }
+
+  if (eve.aanDeBeurt) {
+    eve.beweeg();
+  }
+  else {
+    alice.beweeg();
+    bob.beweeg();
+    eve.aanDeBeurt = true;
+  }
+  
+  if (alice.x == bob.x && alice == bob.y) {
+    bob.beweeg();
+  }
+  
+  eve.toon();
+  alice.toon();
+  bob.toon();
+  
+  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob) || eve.staatOp(bommenArray)) {
+    background('red');
+    fill('white');
+    text("Je hebt verloren!",30,300);
+    noLoop();
+  }
+
+  if (eve.gehaald) {
+    background('green');
+    fill('white');
+    text("Je hebt gewonnen!",30,300);
+    noLoop();
+  }
+}
+
+
+
+class Raster {
+  constructor(r,k) {
+    this.aantalRijen = r;
+    this.aantalKolommen = k;
+    this.celGrootte = null;
+  }
+  
+  berekenCelGrootte() {
+    this.celGrootte = canvas.width / this.aantalKolommen;
+  }
+  
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij<this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom<this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
+
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+  
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
